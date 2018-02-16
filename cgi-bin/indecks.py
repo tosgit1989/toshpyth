000890f77ac7cfe8983bdac89912c1d77dc7b541
tosgit1989 <purannaa1989@gmail.com>
2018-02-17 07:15:50 +0900

    pasta

--- cgi-bin/indecks.py
+++ cgi-bin/indecks.py
@@ -1,5 +1,5 @@
 # !/usr/bin/env python3
 print('Content-Type: text/html; charset=utf-8')
 print('<html><head><meta charset="utf-8"><body>')
-print('aassss')
+print('aassssdddd')
 print('</body></head></html>')
