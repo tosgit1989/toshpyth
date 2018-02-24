d8023c4916bc7e8dcd8447d4d3574899567970fc
tosgit1989 <purannaa1989@gmail.com>
2018-02-24 18:39:18 +0900

    コミットテスト5

--- cgi-bin/indecks.py
+++ cgi-bin/indecks.py
@@ -1,5 +1,5 @@
 # !/usr/bin/env python3
 print('Content-Type: text/html; charset=utf-8')
 print('<html><head><meta charset="utf-8"><body>')
-print('bin-bin-bin-bin')
+print('bin-bin-bin-bin-dd')
 print('</body></head></html>')
